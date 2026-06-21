8f12870bf4dc982db3b19c5f0a510cef2f00c062
Navaneeth <65028198+testdrivenmind@users.noreply.github.com>
2026-06-21 08:31:24 +0800

    Add: minimum-stack - submission-12

---
+++ Data Structures & Algorithms/minimum-stack/submission-12.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.my_stack = []
+
+
+    def push(self, val: int) -> None:
+        if not self.my_stack:
+            current_min = val
+            self.my_stack.append((val, current_min))
+            return 
+        current_min = min(val, self.my_stack[-1][1])
+        self.my_stack.append((val, current_min))
+
+    def pop(self) -> None:
+        if self.my_stack:
+            self.my_stack.pop()
+
+        
+
+    def top(self) -> int:
+        if self.my_stack:
+            return self.my_stack[-1][0]
+    
+
+    def getMin(self) -> int:
+        if self.my_stack:
+            return self.my_stack[-1][1]
+    
