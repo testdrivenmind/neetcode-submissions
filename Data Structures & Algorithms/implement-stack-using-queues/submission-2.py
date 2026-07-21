61200d39a8c30480d3531d2fc23d3289b321b085
Navaneeth <65028198+testdrivenmind@users.noreply.github.com>
2026-07-21 08:13:46 +0800

    Add: implement-stack-using-queues - submission-2

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-2.py
@@ -0,0 +1,31 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+        self.sq = deque() #sq - stack queue
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        q_size = len(self.q) - 1 # except the last element
+        for _ in range(q_size):
+            removed_element = self.q.popleft()
+            self.q.append(removed_element)
+        
+
+    def pop(self) -> int:
+        return self.q.popleft()
+        
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
